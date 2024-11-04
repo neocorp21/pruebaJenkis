681643509b4b451010d9999b024583b9260d42a8
Alberto <alberto.limas.contractor@bbva.com>
2024-11-03 19:47:28 -0500

    fix  jenkis server

--- src/main/java/com/example/demojenkis/controller/MetodoController.java
+++ src/main/java/com/example/demojenkis/controller/MetodoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/metodo")
+@RequestMapping
 public class MetodoController {
     @GetMapping("/hola")
     public String metodo(){
